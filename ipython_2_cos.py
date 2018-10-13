891c623523c4787f1fcc2a035be3ae78b395ba99
ChisIlya <chisilia12@gmail.com>
2018-10-13 20:44:16 +0300

    Вычисление косинуса

---
+++ ipython_2_cos.py
@@ -0,0 +1,19 @@
+﻿import math
+
+iterations = 20
+
+def my_cos(x):
+    """
+    """
+    partial_sum = 0
+    x_pow = 1
+    for n in range(0, iterations, 2):
+
+        minus = x_pow * x ** 2 / (2 * n + 1 ) / (2 * n + 2)
+        partial_sum += x_pow - minus
+        x_pow = minus * x ** 2 / (2 * n + 3) / (2 * n + 4 )
+    
+    return partial_sum
+
+print(my_cos(0.125))
+print(math.cos(0.125))
