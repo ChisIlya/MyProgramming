fd0950aa25ff725317a25369c89597d35748b95d
ChisIlya <chisilia12@gmail.com>
2018-12-09 22:15:15 +0300

    1

---
+++ ip_7A_Euclidean_algorithm.py
@@ -0,0 +1,13 @@
+def gcd(a,b):
+    if (b==0):
+        return a
+    else: 
+        return gcd(b, a % b)
+
+a = input()
+b = input()
+
+a = int(a)
+b = int(b)
+
+print(gcd(a,b))
---
+++ ip_7B_is_prime.py
@@ -0,0 +1,29 @@
+import math
+import functools
+
+def is_prime(a):
+    x = True
+    if a < 2:
+        x = False
+    elif a == 2:
+        x = True
+    elif a % 2 == 0:
+        x = False
+    else:
+        for i in range(3, int(math.sqrt(a) + 1), 2):
+            if(a % i == 0):
+                x = False
+                break
+    return x
+
+a = input()
+a = int(a)
+print(is_prime(a))
+
+@functools.lru_cache()
+def is_prime_cached(a):
+    return is_prime(a)
+
+%timeit is_simple(500)
+%timeit is_simple_cached(500)
+print(is_prime_cached(500))
