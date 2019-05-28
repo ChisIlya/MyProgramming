d61f7277c529a9d3389d3fd899c4a0538f5dc0f2
ChisIlya <43726515+ChisIlya@users.noreply.github.com>
2019-05-28 16:42:53 +0300

    Update ip_DeviationFromFive_ExpStatistics.py

--- ip_DeviationFromFive_ExpStatistics.py
+++ ip_DeviationFromFive_ExpStatistics.py
@@ -3,6 +3,11 @@ import xlrd
 from scipy import stats
 
 devfromfive = pd.read_excel('C:\Laba\lab_1.01.xlsx', sheet_name=0)
+devfromfive.plot()
+
+devfromfive.plot.kde()
+
+
 print(stats.kstest(devfromfive, 'norm', (devfromfive.mean(),devfromfive.std())))
 
 #https://github.com/ChisIlya/MyProgramming/blob/master/lab_1.01.xlsx
